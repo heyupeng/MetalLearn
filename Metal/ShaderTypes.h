b2f50475213c6b6f95606c78e36ee796af026289
peng <13750523250@163.com>
2021-07-23 16:03:57 +0800

    Update

---
+++ Metal/ShaderTypes.h
@@ -0,0 +1,11 @@
+//
+//  ShaderTypes.h
+//  MetalDemo
+//
+//  Created by Peng on 2021/7/20.
+//
+
+#ifndef ShaderTypes_h
+#define ShaderTypes_h
+
+#endif /* ShaderTypes_h */
--- MetalDemo/ViewController.swift
+++ MetalDemo/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import Metal
+import MetalKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mtkview: MTKView!
+    
+    var render: Render!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        render = Render(mtkView: mtkview)
+        mtkview.clearColor = MTLClearColor(red: 0.5, green: 0.5, blue: 0, alpha: 1)
+        mtkview.preferredFramesPerSecond = 25
     }
 
-
+    @IBAction func switchAction(_ sender: UISwitch) {
+        let index = sender.tag
+        
+        switch index {
+        case 0:
+            Render.Axis.xFlag = sender.isOn
+        case 1:
+            Render.Axis.yFlag = sender.isOn
+        case 2:
+            Render.Axis.zFlag = sender.isOn
+        case 3:
+            Render.Axis.xFlag = sender.isOn
+            Render.Axis.yFlag = sender.isOn
+        case 4:
+            Render.Axis.xFlag = sender.isOn
+            Render.Axis.zFlag = sender.isOn
+        case 5:
+            Render.Axis.yFlag = sender.isOn
+            Render.Axis.zFlag = sender.isOn
+        case 6:
+            Render.Axis.xFlag = sender.isOn
+            Render.Axis.yFlag = sender.isOn
+            Render.Axis.zFlag = sender.isOn
+        default:
+            break
+        }
+    }
+    
+    @IBAction func resetAction(_ sender: UIButton) {
+        Render.Axis.xFlag = false
+        Render.Axis.yFlag = false
+        Render.Axis.zFlag = false
+        
+        Render.Axis.x = 0
+        Render.Axis.y = 0
+        Render.Axis.z = 0
+    }
+    
 }
 
